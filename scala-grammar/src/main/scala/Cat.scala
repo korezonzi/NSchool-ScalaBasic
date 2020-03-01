75f56b84d43cea8257ebfb016fe23bd945e5e5e8
Fuma_Tsuchiya <korezonzip@gmail.com>
2020-03-02 01:57:17 +0900

    how to class and instance

--- scala-grammar/src/main/scala/Cat.scala
+++ scala-grammar/src/main/scala/Cat.scala
@@ -1,9 +1,14 @@
-class Cat(name: String) {
+object Cat{
   def main(args: Array[String]): Unit = {
-    println(greet("ぽち"))
+    //println()
+    val cat = new Cat("ぽち")
+    cat.greet()
+    //println(cat)
   }
 
-  def greet(name: String): Unit = {
-    println(s"Cat's name is....${name}")
+  class Cat(val name: String) {
+    def greet(): Unit = {
+      println(s"Cat's name is....${name}")
+    }
   }
 }
