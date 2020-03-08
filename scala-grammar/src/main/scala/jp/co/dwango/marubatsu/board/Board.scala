d7c21c8bb1a25c2b715ff89315599fa9002e5e37
Fuma_Tsuchiya <korezonzip@gmail.com>
2020-03-09 08:41:25 +0900

    doing board game.

---
+++ scala-grammar/src/main/scala/jp/co/dwango/marubatsu/board/Board.scala
@@ -0,0 +1,34 @@
+package jp.co.dwango.marubatsu.board
+
+class Board(val cells: Map[(Int, Int), CellState], val next: CellState) {
+  def put(row: Int, column: Int): Board = {
+    new Board(cells +  ((row, column) -> next), getNext(next))
+  }
+
+  private[this] def getNext(current: CellState): CellState = {
+    current match {
+      case Empty => Empty
+      case Maru  => Batsu
+      case Batsu => Maru
+    }
+  }
+
+  def canPut(row: Int, column: Int): Boolean = {
+    cells((row, column)) == Empty
+  }
+
+  override def toString: String = {
+    s"Board($cells, $next)"
+  }
+}
+
+object Board {
+  def apply(): Board = {
+    val keyValues =
+      for{
+        row    <- 0 to 2
+        column <- 0 to 2
+      } yield (row, column) -> Empty
+    new Board(keyValues.toMap, Maru)
+  }
+}
